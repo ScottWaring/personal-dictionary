261d0df39ffe4a201d212a101b266f2500755236
Scott Waring <scott.waring@gmail.com>
2019-01-24 22:22:31 -0500

    heroku key addition

--- src/app/word.service.ts
+++ src/app/word.service.ts
@@ -71,7 +71,7 @@ export class WordService {
 
 //fetch to api based on user inputs, and pass returned object to function
   getWord(word) {
-    return  this.http.get(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {headers: { "X-RapidAPI-Key": API KEY GOES HERE}})
+    return  this.http.get(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {headers: { "X-RapidAPI-Key": WORDS_KEY}})
       .subscribe(wordRes => {this.changeWordObj(wordRes), console.log(wordRes)});
   }
 
