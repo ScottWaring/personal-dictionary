ddd83c50bc013b9cd08361c7347ae0e43679adaf
Scott Waring <scott.waring@gmail.com>
2019-01-25 20:51:36 -0500

    Revert "procfile added"
    This reverts commit 48e10f9e80b230a930d937cf2f83fc52baca1579.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to Your Personal Dictionary!');
+    expect(page.getTitleText()).toEqual('Welcome to personal-dictionary!');
   });
 });
